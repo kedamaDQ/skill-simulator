d0d05085bb15c9658100910f0ed07cca28efe82c
kedamaDQ <kedama@foresdon.jp>
2018-11-05 11:58:56 +0900

    Update react-select to 2.2.1
    Reset settings for react-select.

--- src/components/owned_selector.js
+++ src/components/owned_selector.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import ReactSelect from 'react-select';
-import 'react-select/dist/react-select.css';
 
 const OwnedSelector = (props) => {
 
